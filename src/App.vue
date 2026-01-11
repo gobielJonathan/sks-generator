f1849da11de25b69c1ae04121ef4a4bfe0a3694d
jonathan.gobiel <jonathan.gobiel@dana.id>
2026-01-11 12:07:15 +0700

    chore: remove upload, convert into table li st

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,10 @@
 <script setup lang="ts">
 import { nextTick, ref, watchEffect } from 'vue';
-import readXlsxFile from 'read-excel-file'
 import * as XLSX from 'xlsx'
-import templateCSV from './assets/template-sks-ulang.xlsx'
 
 type Subject = {
-  kode_mata_kuliah: boolean,
-  nama_mata_kuliah: boolean,
+  kode_mata_kuliah: string,
+  nama_mata_kuliah: string,
   sks: number
   is_lab: boolean,
   is_taken_now: boolean
@@ -16,7 +14,11 @@ type PeriodSubject = {
   [period: string]: Subject[]
 }
 
-const FAILED_SUBJECT_STORAGE_KEY = 'failed-subject';
+const FAILED_SUBJECT_STORAGE_KEY = 'list-subject';
+const FAILED_SKS_STORAGE_KEY = 'sks-subject';
+const FAILED_SEMESTER_STORAGE_KEY = 'semester-subject';
+
+
 const ROMAN_REGEX = /\b[IVXLCDM]+\b/i
 const MAX_SEMESTER = 14;
 const MAX_SKS = 146;
@@ -26,32 +28,35 @@ const GANJIL = 1
 const MIN_SKS_TAKE_ENRICHMENT = 60
 const SKS_ENRICHMENT = 20
 const ENRICHMENT_SUBJECT_CODE = 'Enrichment'
+
+const alreadyEnrichmentFirst = ref(false);
+const alreadyEnrichmentSecond = ref(false);
+const alreadyThesis = ref(false);
+
 const THESIS_SUBJECT_CODE = 'Thesis'
 const SKS_THESIS = 6
 
-const runningSemester = ref(1);
-const runningSKS = ref(1);
+const runningSemester = ref(Number(localStorage.getItem(FAILED_SEMESTER_STORAGE_KEY) || '1'));
+const runningSKS = ref(Number(localStorage.getItem(FAILED_SKS_STORAGE_KEY) || '1'));
+
 const generateSKSTableRef = ref<HTMLElement | null>(null);
-const failedSubject = ref<PeriodSubject>(safeParseJSON(localStorage.getItem(FAILED_SUBJECT_STORAGE_KEY) || '{}'));
+const generateListSKSTableRef = ref<HTMLElement | null>(null);
+const groupFailedSubject = ref<PeriodSubject>({});
+const failedSubjects = ref<(Subject & { period: string })[]>(
+  safeParseJSON(localStorage.getItem(FAILED_SUBJECT_STORAGE_KEY) || '[]')
+)
 const futureSubject = ref<PeriodSubject>({});
 
 watchEffect(() => {
-  localStorage.setItem(FAILED_SUBJECT_STORAGE_KEY, JSON.stringify(failedSubject.value));
+  localStorage.setItem(FAILED_SUBJECT_STORAGE_KEY, JSON.stringify(failedSubjects.value));
 });
 
-const previewUploadRepeatFile = async (file: FileList) => {
-  const _file = file.item(0);
-  if (!_file) return;
-  try {
-    const rows = await readXlsxFile(_file);
-    const data = transformExcelRows(rows)
-    console.log(rows, data)
-    failedSubject.value = data as unknown as PeriodSubject
-  } catch (error) {
-    alert('Gagal membaca file. Pastikan format file sesuai dan coba lagi.')
-  }
-}
-
+watchEffect(() => {
+  localStorage.setItem(FAILED_SKS_STORAGE_KEY, String(runningSKS.value));
+});
+watchEffect(() => {
+  localStorage.setItem(FAILED_SEMESTER_STORAGE_KEY, String(runningSemester.value));
+});
 
 function safeParseJSON(jsonString: string) {
   try {
@@ -63,20 +68,30 @@ function safeParseJSON(jsonString: string) {
 
 async function downloadCSV() {
   await nextTick() // ensure DOM is rendered
-
-  const ws = XLSX.utils.table_to_sheet(generateSKSTableRef.value)
+  const ws = XLSX.utils.table_to_sheet(generateListSKSTableRef.value)
   const wb = XLSX.utils.book_new()
 
-  XLSX.utils.book_append_sheet(wb, ws, 'Grades')
-  XLSX.writeFile(wb, 'grades.xlsx')
+  XLSX.utils.book_append_sheet(wb, ws, 'sks-plan')
+  XLSX.utils.sheet_add_dom(ws, generateSKSTableRef.value, { origin: -1 })
+  XLSX.writeFile(wb, 'sks-plan.xlsx')
 }
 
 const generateFutureSubject = () => {
 
+  groupFailedSubject.value = failedSubjects.value.reduce((acc, curr) => {
+    acc[curr.period] ??= []
+    const _curr = {
+      ...curr,
+      kode_mata_kuliah: curr.kode_mata_kuliah.toLowerCase().includes("enrichment") ? ENRICHMENT_SUBJECT_CODE : curr.kode_mata_kuliah,
+    }
+    acc[curr.period].push(_curr)
+    return acc
+  }, {})
+
   const subjectPerSemester: Record<number, Subject[]> = { [GENAP]: [], [GANJIL]: [] }
 
   //seperate all subjects by ganjil / genap semester
-  for (const [period, subjects] of Object.entries(failedSubject.value)) {
+  for (const [period, subjects] of Object.entries(groupFailedSubject.value)) {
     const periodInfo = parsePeriodString(period)
     if (!periodInfo) continue
     subjectPerSemester[periodInfo.semester_number % 2 === 1 ? GANJIL : GENAP].push(...subjects)
@@ -96,6 +111,16 @@ const generateFutureSubject = () => {
   const takenNowSubjects = subjectPerSemester[getSemesterType(_runningSemester)]?.filter(s => s.is_taken_now) || []
   if (takenNowSubjects.some(s => s.kode_mata_kuliah.includes('Enrichment'))) {
     _countEnrichmentTaken++
+  } else {
+    if (alreadyEnrichmentFirst.value) {
+      _countEnrichmentTaken++
+    }
+    if (alreadyEnrichmentSecond.value) {
+      _countEnrichmentTaken++
+    }
+    if (alreadyThesis.value) {
+      _isAlreadyGetThesis = true
+    }
   }
 
   _futureSubject[_period].push(...takenNowSubjects)
@@ -109,8 +134,8 @@ const generateFutureSubject = () => {
   _runningSemester++
   if (_runningSemester % 2 === 0) _incrementYear++
 
-  while (_runningSemester < MAX_SEMESTER && _runningSKS < MAX_SKS) {
-    if (subjectPerSemester[GENAP].length === 0 && subjectPerSemester[GANJIL].length === 0) {
+  while (_runningSemester <= MAX_SEMESTER && _runningSKS < MAX_SKS) {
+    if (subjectPerSemester[GENAP]?.length === 0 && subjectPerSemester[GANJIL]?.length === 0) {
       //no more subjects to take
       break
     }
@@ -171,46 +196,9 @@ const generateFutureSubject = () => {
     if (_runningSemester % 2 === 0) _incrementYear++
     _runningSemester++
   }
-
+  console.log('_futureSubject', _futureSubject);
   futureSubject.value = _futureSubject
-}
 
-function transformExcelRows(rows: any[][]) {
-  const result: Record<string, any[]> = {}
-  let currentSemester = null
-
-  // skip header row
-  for (let i = 1; i < rows.length; i++) {
-    const row = rows[i] || []
-
-    const [
-      semester,
-      _kode,
-      nama,
-      sks
-    ] = row
-    const kode = _kode || nama
-
-    // detect new semester
-    if (semester && typeof semester === 'string' && semester.trim()) {
-      currentSemester = semester.trim()
-      if (!result[currentSemester]) {
-        result[currentSemester] = []
-      }
-    }
-
-    // skip invalid rows
-    if (!currentSemester) continue
-    if (!kode || !nama || !sks) continue
-
-    result[currentSemester]?.push({
-      kode_mata_kuliah: String(kode).trim(),
-      nama_mata_kuliah: String(nama).trim(),
-      sks: Number(sks)
-    })
-  }
-
-  return result
 }
 
 function numberToRoman(num: number): string {
@@ -347,14 +335,12 @@ function createPeriodString(
       MAX_SKS }}</span>
   </div>
 
-  <div>
-    <p>upload data pengulangan</p>
-    <a style="margin-right: 8px;" :href="templateCSV" download>Download Template</a>
-
-    <input type="file"
-      accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel"
-      @change="e => previewUploadRepeatFile(e.target.files)">
-
+  <div style="display: flex; align-items: center; column-gap: 8px;">
+    <input type="checkbox" v-model="alreadyEnrichmentFirst" name="" id="enrich-1"> <label for="enrich-1">Encrichment
+      1</label>
+    <input type="checkbox" v-model="alreadyEnrichmentSecond" name="" id="enrich-2"> <label for="enrich-2">Encrichment
+      2</label>
+    <input type="checkbox" v-model="alreadyThesis" name="" id="thesis"> <label for="thesis">Thesis</label>
   </div>
 
   <table class="repeat-table">
@@ -367,33 +353,75 @@ function createPeriodString(
         <th>Kode mata kuliah</th>
         <th>Mata kuliah</th>
         <th>SKS</th>
+        <th></th>
       </tr>
     </thead>
     <tbody>
-      <template v-for="[period, subjects] in Object.entries(failedSubject)">
-        <tr v-for="subject, index in subjects">
-          <td>
-            <input type="checkbox" :checked="subject.is_lab" @change="e => {
-              failedSubject[period][index].is_lab = e.target.checked
-            }">
-            <span style="margin: 0 .2rem;">/</span>
-            <input type="checkbox" :checked="subject.is_taken_now" @change="e => {
-              failedSubject[period][index].is_taken_now = e.target.checked
-            }">
-          </td>
-          <td v-if="index === 0" :rowspan="subjects.length">{{ period }}
-          </td>
-          <td>{{ subject.kode_mata_kuliah }}</td>
-          <td>{{ subject.nama_mata_kuliah }}</td>
-          <td>{{ subject.sks }}</td>
-        </tr>
-      </template>
+      <tr v-for="subject, index in failedSubjects" :key="index">
+        <td>
+          <input type="checkbox" :checked="subject.is_lab" @change="e => {
+
+          }">
+          <span style="margin: 0 .2rem;">/</span>
+          <input type="checkbox" :checked="subject.is_taken_now" @change="e => {
+          }">
+        </td>
+        <td>
+          <input type="text" v-model="subject.period">
+        </td>
+        <td>
+          <input type="text" v-model="subject.kode_mata_kuliah">
+        </td>
+        <td>
+          <input type="text" v-model="subject.nama_mata_kuliah">
+        </td>
+        <td>
+          <input type="text" v-model.number="subject.sks">
+        </td>
+        <td>
+          <button @click="() => {
+            failedSubjects = failedSubjects.filter((_, idx) => index !== idx)
+          }" class="btn">X</button>
+        </td>
+      </tr>
 
     </tbody>
+
+    <tfoot>
+      <tr>
+        <td colspan="6">
+          <button @click="failedSubjects.push({})" class="add-button btn">tambahkan data</button>
+        </td>
+      </tr>
+    </tfoot>
   </table>
 
   <div style="margin-top: 1rem;">
     <button @click="generateFutureSubject">Hasilkan Rencana Pengambilan mata kuliah</button>
+
+    <table ref="generateListSKSTableRef" class="repeat-table">
+      <thead>
+        <tr>
+          <th>Periode / Semester</th>
+          <th>Kode mata kuliah</th>
+          <th>Mata kuliah</th>
+          <th>SKS</th>
+        </tr>
+      </thead>
+      <tbody>
+        <template v-for="[period, subjects] in Object.entries(groupFailedSubject)">
+          <tr v-for="subject, index in subjects">
+            <td v-if="index === 0" :rowspan="subjects.length">{{ period }}
+            </td>
+            <td>{{ subject.kode_mata_kuliah }}</td>
+            <td>{{ subject.nama_mata_kuliah }}</td>
+            <td>{{ subject.sks }}</td>
+          </tr>
+        </template>
+      </tbody>
+    </table>
+
+    <p>Rencana pengambilan mata kuliah</p>
 
     <table ref="generateSKSTableRef" class="repeat-table" id="rencana-sks-table">
       <thead>
@@ -413,7 +441,7 @@ function createPeriodString(
             <td>{{ subject.nama_mata_kuliah }}</td>
             <td>{{ subject.sks }}</td>
           </tr>
-          <tr>
+          <tr v-if="subjects.length > 0">
             <td colspan="3"></td>
             <td><b>{{subjects.reduce((acc, subject) => acc + subject.sks, 0)}}</b></td>
           </tr>
@@ -434,6 +462,17 @@ function createPeriodString(
 
 
 <style scoped>
+.btn {
+  cursor: pointer;
+  width: 100%;
+  border: none;
+}
+
+.add-button {
+
+  padding: .5rem 0;
+}
+
 .repeat-table {
   margin-top: 1rem;
   border-collapse: collapse;
--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-   assetsInclude: ['**/*.xlsx'],
+  assetsInclude: ['**/*.xlsx'],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
